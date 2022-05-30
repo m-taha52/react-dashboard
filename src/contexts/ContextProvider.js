b2b9dda7735fd00c636d5f158dd02895d08c112f
Mohammed Taha Alibhai <mot1922092@sicsr.ac.in>
2022-05-30 11:27:08 +0300

    feat:created isClicked, setIsClicked and handleClick function
     The isClicked and setIsClicked are used to display some content when each icon is clicked, the handleclick function changes the state to ensure only the content for the icon that is clicked is displayed

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,11 +12,19 @@ const initialState = {
 export const ContextProvider = ({children}) => {
 
     const [activeMenu, setActiveMenu] = useState(true)
+    const [isClicked, setIsClicked] = useState(initialState)
+
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]: true})
+    }
     return (
         <StateContext.Provider
         value={{
-            activeMenu: activeMenu,
-            setActiveMenu: setActiveMenu,
+            activeMenu,
+            setActiveMenu,
+            isClicked,
+            setIsClicked,
+            handleClick,
 
         }}
         >
