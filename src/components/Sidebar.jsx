50896a02a615e90d656b14146f82f1b9db193406
Mohammed Taha Alibhai <mot1922092@sicsr.ac.in>
2022-05-30 11:22:43 +0300

    fix: close icon not appearing on medium devices

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
                   <SiShopware /> <span> Shoppy </span>
               </Link>
               <TooltipComponent content="Menu" position="BottomCenter">
-                <button type='button' onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
+                <button type='button' onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block ">
                   <MdOutlineCancel /> 
 
                 </button>
